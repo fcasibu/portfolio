656c4174fe329560d9f037258129204d290c6477
nevz <projectnevz@gmail.com>
2022-06-09 00:56:40 +0800

    Add props to StyledThemeIcon for conditional styling

--- src/components/Header/ThemeIcon.tsx
+++ src/components/Header/ThemeIcon.tsx
@@ -2,14 +2,14 @@ import { FiSun, FiMoon } from 'react-icons/fi'
 import { StyledThemeIcon } from './styles'
 
 interface Props {
-  theme: string;
-  themeChange(): void;
+  theme: string
+  themeChange(): void
 }
 
 export const ThemeIcon = ({ theme, themeChange }: Props) => {
-  return <StyledThemeIcon onClick={themeChange}>
-    <span>
-      {theme === 'light' ? <FiSun title="Sun"/> : <FiMoon title="Moon"/>}
-    </span>
-  </StyledThemeIcon>
+  return (
+    <StyledThemeIcon onClick={themeChange} theme={theme}>
+      <span>{theme === 'light' ? <FiSun title="Sun" /> : <FiMoon title="Moon" />}</span>
+    </StyledThemeIcon>
+  )
 }
