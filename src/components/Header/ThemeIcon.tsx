ef69912913e69cbe33ac92d3b4fb20e8a9957f0e
nevz <projectnevz@gmail.com>
2022-06-08 23:32:31 +0800

    Add ThemeIcon component

---
+++ src/components/Header/ThemeIcon.tsx
@@ -0,0 +1,15 @@
+import { FiSun, FiMoon } from 'react-icons/fi'
+import { StyledThemeIcon } from './styles'
+
+interface Props {
+  theme: string;
+  themeChange(): void;
+}
+
+export const ThemeIcon = ({ theme, themeChange }: Props) => {
+  return <StyledThemeIcon onClick={themeChange}>
+    <span>
+      {theme === 'light' ? <FiSun title="Sun"/> : <FiMoon title="Moon"/>}
+    </span>
+  </StyledThemeIcon>
+}
