1ee1dfa4cbc98f5408d72aca8e8efaebc476528c
nevz <projectnevz@gmail.com>
2022-06-08 23:31:58 +0800

    Add themeChangeHandler for ThemeIcon component
    changes the class of html to light/dark

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,20 @@
+import * as React from 'react';
 import { List } from './List'
 import { Nav, StyledHeader } from './styles'
+import { ThemeIcon } from './ThemeIcon'
 
 const Header = () => {
+  const [theme, setTheme] = React.useState('light');
+
+  const themeChangeHandler = () => {
+    document.documentElement.classList.toggle('dark');
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
   return (
     <StyledHeader>
       <Nav>
         <List />
+        <ThemeIcon theme={theme} themeChange={themeChangeHandler}/>
       </Nav>
     </StyledHeader>
   )
