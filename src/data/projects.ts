273068942dfdbdcd74377bf24b1e12b0ef79c753
nevz <projectnevz@gmail.com>
2022-06-10 00:22:14 +0800

    Add link and repo fields

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,10 @@
 export const projects = [
   {
     title: 'Battleship',
-    description: 'Single player battleship game, where the player and computer battles out who sinks the most ships first',
+    description:
+      'Single player battleship game, where the player and computer battles out who sinks the most ships first',
+    repo: 'https://github.com/nevz9/battleship',
+    live: 'https://nevz9.github.io/battleship/',
     image: require('../assets/battleship.png'),
     technologies: [
       require('../assets/html.svg'),
@@ -13,16 +16,21 @@ export const projects = [
   {
     title: 'Fakegram',
     description: 'Instagram clone, with features including posting, following, liking, etc',
+    repo: 'https://github.com/nevz9/fakegram-react',
+    live: 'https://nevz9.github.io/fakegram-react/',
     image: require('../assets/fakegram.png'),
     technologies: [
       require('../assets/react.svg'),
       require('../assets/firebase.svg'),
-      require('../assets/jest.svg')
+      require('../assets/css.svg')
     ]
   },
   {
     title: 'Photo Tagging Application',
-    description: 'Photo Tagging Application. Three maps. Three Characters. Find all three as fast as you can!',
+    description:
+      'Photo Tagging Application. Three maps. Three Characters. Find all three as fast as you can!',
+    repo: 'https://github.com/nevz9/photo-tagging-app',
+    live: 'https://photo-tagging-app-852b2.web.app/#/',
     image: require('../assets/photo-tagging.png'),
     technologies: [
       require('../assets/react.svg'),
@@ -33,12 +41,15 @@ export const projects = [
   },
   {
     title: 'Shopping Cart',
-    description: 'Shopping Cart. First time implementing React Router and imitating the logic of shoping and adding to a cart',
-    image: require('../assets/fakegram.png'),
+    description:
+      'Shopping Cart. First time implementing React Router and imitating the logic of shoping and adding to a cart',
+    repo: 'https://github.com/nevz9/shopping-cart',
+    live: 'https://nevz9.github.io/shopping-cart/',
+    image: require('../assets/shopping-cart.png'),
     technologies: [
       require('../assets/react.svg'),
       require('../assets/css.svg'),
       require('../assets/jest.svg')
     ]
-  },
-]
+  }
+];
