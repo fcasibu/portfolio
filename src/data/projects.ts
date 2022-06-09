868d93885567d2c141361775ee9f87c857300289
nevz <projectnevz@gmail.com>
2022-06-09 21:26:25 +0800

    Add assets and data for projects

---
+++ src/data/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: 'Battleship',
+    description: 'Single player battleship game, where the player and computer battles out who sinks the most ships first',
+    image: require('../assets/battleship.png'),
+    technologies: [
+      require('../assets/html.svg'),
+      require('../assets/css.svg'),
+      require('../assets/javascript.svg'),
+      require('../assets/jest.svg')
+    ]
+  },
+  {
+    title: 'Fakegram',
+    description: 'Instagram clone, with features including posting, following, liking, etc',
+    image: require('../assets/fakegram.png'),
+    technologies: [
+      require('../assets/react.svg'),
+      require('../assets/firebase.svg'),
+      require('../assets/jest.svg')
+    ]
+  },
+  {
+    title: 'Photo Tagging Application',
+    description: 'Photo Tagging Application. Three maps. Three Characters. Find all three as fast as you can!',
+    image: require('../assets/photo-tagging.png'),
+    technologies: [
+      require('../assets/react.svg'),
+      require('../assets/firebase.svg'),
+      require('../assets/jest.svg'),
+      require('../assets/css.svg')
+    ]
+  },
+  {
+    title: 'Shopping Cart',
+    description: 'Shopping Cart. First time implementing React Router and imitating the logic of shoping and adding to a cart',
+    image: require('../assets/fakegram.png'),
+    technologies: [
+      require('../assets/react.svg'),
+      require('../assets/css.svg'),
+      require('../assets/jest.svg')
+    ]
+  },
+]
