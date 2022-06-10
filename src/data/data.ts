95ca0db942d14cdf4475c9b8c350a7c81a63fdb7
nevz <projectnevz@gmail.com>
2022-06-11 02:21:17 +0800

    Add data for About

---
+++ src/data/data.ts
@@ -0,0 +1,23 @@
+export const aboutData = {
+  titles: { about: 'About me', lt: 'Languages and Tools', socials: 'Socials' },
+  avatar: require('../assets/avatar-default-icon.png'),
+  info1:
+    'Hello there! I am Francis Eugene Casibu, a full stack web developer based in the Philippines. An undergrad studying Information Technology. I have a passion for developing web appplications, and fully committed to lifelong learning.',
+  info2:
+    'I started my Web Development journey on January 8, 2022 and has been going strong ever since. When I am off coding, I enjoy reading light novels, working out, playing basketball, and helping other learners achieve their goal in The Odin Project discord',
+  ltIcons: [
+    require('../assets/html.svg'),
+    require('../assets/css.svg'),
+    require('../assets/javascript.svg'),
+    require('../assets/typescript.svg'),
+    require('../assets/react.svg'),
+    require('../assets/node.svg'),
+    require('../assets/mongodb.svg'),
+    require('../assets/jest.svg'),
+    require('../assets/firebase.svg'),
+    require('../assets/git.svg'),
+    require('../assets/webpack.svg')
+  ]
+};
+
+export const contactData = {};
