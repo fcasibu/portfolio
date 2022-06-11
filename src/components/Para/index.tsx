59b500e03c04b6d7eb98eb5604ff5bcdd0d0db6a
nevz <projectnevz@gmail.com>
2022-06-11 16:54:27 +0800

    Move Para and Title to their own directory

---
+++ src/components/Para/index.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Para = styled.p`
+  font-size: 0.9rem;
+
+  @media (max-width: 840px) {
+    font-size: 0.8rem;
+  }
+
+  @media (max-width: 760px) {
+    font-size: 0.75rem;
+  }
+`;
---
+++ src/components/Title/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  font-size: 1.4rem;
+  font-weight: normal;
+  margin: 0.5rem 0;
+`;
